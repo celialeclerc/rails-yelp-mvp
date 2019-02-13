23af78c0d5a328d6bae3290c7eaef74ae0e6365f
Celia Leclerc <leclerc.celia@yahoo.fr>
2019-02-13 15:21:05 +0100

    Fini

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    category:      'french'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    category:      'french'
+  },
+  {
+    name:         'Bocuse',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    category:      'french'
+  },
+  {
+    name:         'Ober Mamma',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    category:      'french'
+  },
+  {
+    name:         'PNY',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    category:     'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
